10556fc4f8ff70bc2242d9a5a8aae9f58edddbab
rodrigo debian <correo.rodrigo@gmail.com>
2022-07-26 10:25:46 -0500

    adding user views

--- themes/vex-hugo/assets/js/script.js
+++ themes/vex-hugo/assets/js/script.js
@@ -48,4 +48,95 @@ $(window).on('load', function () {
     ]
   });
 
+  
+
+  let showButton = (isCart) =>
+  {
+    if (isCart) {
+      $("button.snipcart-checkout").hide();
+      $("button.snipcart-pagar").show();
+    }else{
+      $("button.snipcart-checkout").show();
+      $("button.snipcart-pagar").hide();
+    }
+  }
+  
+  let loc = window.location.href;
+  showButton(loc.indexOf("#/cart") !== -1);
+    
+  document.addEventListener('snipcart.ready', () => {
+    // 
+    // console.dir(Snipcart);
+   
+    Snipcart.events.on('theme.routechanged', (routesChange) => {
+      let isCart = routesChange.to.indexOf("/cart") !== -1;
+      showButton(isCart);
+    });
+
+    $('span.snipcart-cart-summary-fees__amount').on("change", function () {
+      alert("changed!");
+    });
+    
+  });
+
+  $("#btnPagar").on("click", () => {
+    let pedido = createQr();
+    pedido.get()[0].scrollIntoView({ behavior: "smooth" });    
+  });
+
+  let createQr = ()=> {
+    $("#dPedido").remove();
+    let items = [];
+    $("div.snipcart-item-line__container").each((i, it) => {
+      let src = $(it).find("img:first").attr("src");
+      //console.log(src);
+      let start = src.lastIndexOf("/") + 1;
+      const regex = /.jpg/i;
+      let item = src.substr(start).replace(regex, '');
+      
+      let quantity = $(it)
+                    .find("div.snipcart-item-quantity__quantity")
+                    .find("span:first").text();
+
+      items.push({
+        i: item,
+        q: quantity
+      });
+    });
+
+    let pedido = $(`
+      <div id="dPedido" class="alert alert-success" role="alert">        
+        <p>
+          Gracias por comprar con nosotros, para completar
+          tu compra, envía un WhatsApp con la foto del código QR de tú
+          pedido. Al número 452 201 8336 o dándo click en éste
+          link <a style="color: hotpink;display:inline;" href="https://wa.me/4522018336?text=Pedido,">enviar pedido<a/>.
+          
+          <div id="qrcode" style="display: inline-block"></div>
+        </p>        
+      </div>`
+    );
+    
+    pedido.css("padding","10px");
+    pedido.css("margin-top","20px");
+    pedido.css("margin-bottom","70px");
+    pedido.css("border","2 px dashed goldenrod");
+    pedido.css("background-color","white");
+    pedido.insertAfter("div.snipcart-cart__footer");
+
+    let qrcode = new QRCode("qrcode", {
+      text: JSON.stringify(items),
+      width: 256,
+      height: 256,
+      colorDark : "#000000",
+      colorLight : "#ffffff",
+      correctLevel : QRCode.CorrectLevel.H
+    });
+
+    return pedido;
+  };
+  
+
+  
+
 })(jQuery);
